37722ddbbce042008bc04df0d359c0a30b07ddbe
eugenebudzishyn <ybudzishyn@gmail.com>
2026-03-13 15:38:50 +0400

    taskInProgress

--- src/main.js
+++ src/main.js
@@ -2,21 +2,27 @@ import readline from "readline"
 import fs from "fs/promises"
 import stream from "stream"
 
-function main(){
+async function main(){
     const rl = readline.createInterface(process.stdin, process.stdout);
-
-    function askQuestion(){
+    console.log("Welcome to Data Processing CLI!");
+    let currentDir = await fs.realpath(".")
+    async function askQuestion(currentDir){
+        console.log(`You are currently in ${currentDir}`);
         rl.question("> ", (ans) => {
             if (ans == ".exit"){
                 rl.close();
                 console.log("Thank you for using Data Processing CLI!");
+            } else if (ans == "up"){
+                
+            } else if (ans == "ls"){
+
             } else {
-                askQuestion();
+                askQuestion(currentDir);
             }
             
         })
     }
-    askQuestion();
+    askQuestion(currentDir);
 }
 
 main();
