aa21ac7808fdd2b7e6a3b4758bd8d2dd4dabf6fa
eugenebudzishyn <ybudzishyn@gmail.com>
2026-03-13 17:09:41 +0400

    taskInProgress

--- src/main.js
+++ src/main.js
@@ -7,16 +7,34 @@ async function main(){
     console.log("Welcome to Data Processing CLI!");
     let currentDir = await fs.realpath(".")
     async function askQuestion(currentDir){
+        const fileList = await fs.readdir(currentDir);
+        
         console.log(`You are currently in ${currentDir}`);
+
         rl.question("> ", (ans) => {
+
             if (ans == ".exit"){
+
                 rl.close();
                 console.log("Thank you for using Data Processing CLI!");
             } else if (ans == "up"){
-                
-            } else if (ans == "ls"){
+                let splited = currentDir.split("\\");
+                splited = splited.slice(0, splited.length - 1);
 
+                currentDir = ""
+
+                for (let path of splited){
+                    currentDir += path + "\\";
+                }
+
+                askQuestion(currentDir.slice(0, currentDir.length - 1));
+            } else if (ans == "ls"){
+                for (let file of fileList){
+                    console.log(file);
+                }
+                askQuestion(currentDir);
             } else {
+
                 askQuestion(currentDir);
             }
             
