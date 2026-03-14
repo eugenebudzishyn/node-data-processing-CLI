ee238959af7f2d2882c696be66398022f65a7c4c
eugenebudzishyn <ybudzishyn@gmail.com>
2026-03-15 00:13:36 +0400

    taskInProgress

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import readline from "readline"
 import fs from "fs/promises"
 import stream from "stream"
 import pa from "path"
+import crypto from "crypto"
 
 async function main(){
     const rl = readline.createInterface(process.stdin, process.stdout);
@@ -15,7 +16,7 @@ async function main(){
         console.log(`You are currently in ${currentDir}`);
         
         rl.question("> ", async (ans) => {
-
+            ans = ans.trim();
             if (ans == ".exit"){
                 rl.close();
                 console.log("Thank you for using Data Processing CLI!");
@@ -47,7 +48,7 @@ async function main(){
 
                 askQuestion(currentDir);
 
-            } else if (ans.trim().split(" ")[0] == "csv-to-json"){
+            } else if (ans.split(" ")[0] == "csv-to-json"){
                 ans = ans.slice(11).trim().split(" ");
                 let trimedArray = [];
                 for (let component of ans){
@@ -60,28 +61,41 @@ async function main(){
                     try {
 
                         let data = (await fs.readFile(ans[1])).toString();
-                        let output = {};
+                        let output = [];
 
                         data = data.split("\n");
 
                         data = data.map(str => str.split(","));
-
-                        for (let obj of data[0]){
-
-                            if (obj.slice(obj.length - 1) === "\r"){
-                                obj = obj.slice(0, obj.length - 1);
-                            }
-
-                            output[obj] = [];
-                        }
-
-                        console.log(output);
-                    } catch {
                         
+                        for (let i = 0; i < data.length; i++){
+                            for (let j = 0; j < data[i].length; j++){
+                                if (data[i][j][data[i][j].length - 1] == "\r"){
+                                    
+                                    data[i][j] = data[i][j].slice(0, data[i][j].length - 1);
+                                }
+                            }
+                        }
+                        // console.log(data);
+                        for (let i = 1; i < data.length; i++){
+                            output.push({});
+                            for (let j = 0; j < data[i].length; j++){
+                                output[i-1][data[0][j]] = data[i][j];
+                            }
+                        }
+                        if (ans.length < 4){
+                            await fs.writeFile(`${ans[1].slice(0, ans[1].length - 4)}.json`, JSON.stringify(output));
+                        } else if (ans[2] == "--output"){
+                            await fs.writeFile(ans[3], JSON.stringify(output));
+                        }
+                        
+                    } catch {
+
                         console.log("Operation Failed");
                     }
                     askQuestion(currentDir);
                 }
+                
+            } else if (ans.split(" ")[0] == "json-to-csv"){
                 
             } else {
                 console.log("Unknown Command");
