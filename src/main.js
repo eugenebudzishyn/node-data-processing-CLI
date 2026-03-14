028aaceee70cc71599a125438b055b93206e613d
eugenebudzishyn <ybudzishyn@gmail.com>
2026-03-14 20:45:37 +0400

    testRun2

--- src/main.js
+++ src/main.js
@@ -1,36 +1,45 @@
 import readline from "readline"
 import fs from "fs/promises"
 import stream from "stream"
-import path from "path"
+import pa from "path"
 
 async function main(){
     const rl = readline.createInterface(process.stdin, process.stdout);
     console.log("Welcome to Data Processing CLI!");
     let currentDir = await fs.realpath(".");
     async function askQuestion(currentDir){
-        const fileList = await fs.readdir(currentDir);
-        
         console.log(`You are currently in ${currentDir}`);
-
-        rl.question("> ", (ans) => {
+        rl.question("> ", async (ans) => {
 
             if (ans == ".exit"){
-
                 rl.close();
                 console.log("Thank you for using Data Processing CLI!");
-            } else if (ans == "up"){
-                currentDir = path.win32.normalize(currentDir + "\\..");
 
-                console.log(currentDir);
+            } else if (ans == "up"){
+                let arrayOfFiles = currentDir.split(pa.sep);
                 
+                currentDir = pa.join(...arrayOfFiles, "..");
                 askQuestion(currentDir);
             } else if (ans == "ls"){
+                const fileList = await fs.readdir(currentDir);
                 for (let file of fileList){
                     console.log(file);
                 }
                 askQuestion(currentDir);
-            } else {
+            } else if (ans.split(" ")[0] == "cd"){
+                ans = ans.slice(2).trim();
 
+                try {
+                    await fs.access(pa.join(currentDir, ans));
+                    await fs.readdir(currentDir);
+                    currentDir = pa.join(currentDir, ans);
+                } catch {
+                    console.log("Operation Failed")
+                } 
+                askQuestion(currentDir);
+
+            } else {
+                console.log("Unknown Command");
                 askQuestion(currentDir);
             }
             
