d7327c34f3e7a5dc6cc338f57dca35c389c4191b
eugenebudzishyn <ybudzishyn@gmail.com>
2026-03-15 22:55:33 +0400

    tasksInProgress

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import fs from "fs/promises"
 import stream from "stream"
 import pa from "path"
 import crypto from "crypto"
+import fsf from "fs"
 
 async function main(){
     const rl = readline.createInterface(process.stdin, process.stdout);
@@ -279,7 +280,124 @@ async function main(){
                 }
                 
                 askQuestion(currentDir);
-            }else {
+            } else if (ans.slice(0,7) == "encrypt"){
+                ans = ans.slice(7).split(" ");
+                let trimedArray = [];
+                for (let component of ans){
+                    if (component.trim() != ""){
+                        trimedArray.push(component);
+                    }
+                }
+                ans = trimedArray;
+
+                let input = "";
+
+                let output = "";
+
+                let password = "";
+                try{
+                    for (let i = 0; i < ans.length; i+=2){
+                        if (ans[i] == "--input"){
+                            input = ans[i+1];
+                        } else if (ans[i] == "--output"){
+                            output = ans[i+1];
+                        } else if (ans[i] == "password"){
+                            password = ans[i+1];
+                        }
+                    }
+                    const salt = crypto.randomBytes(16);
+                    const iv = crypto.randomBytes(12);
+                    const authTag = crypto.randomBytes(16);
+
+                    const key = crypto.pbkdf2Sync(password, salt, 10000, 32, 'sha512');
+
+                    const cipheredText = crypto.createCipheriv("aes-256-gcm", key, iv);
+
+                    const inputStream = fsf.createReadStream(input);
+                    const outputStream = fsf.createWriteStream(output);
+
+                    outputStream.write(salt);
+
+                    outputStream.write(iv);
+
+                    inputStream.pipe(cipheredText).pipe(outputStream);
+                    
+                    outputStream.on("error", (error)=> {
+                        console.log(error);
+                    })
+                    
+                    outputStream.write(authTag);
+                } catch {
+                    console.log("Operation Failed");
+                }
+                askQuestion(currentDir);  
+            } else if (ans.slice(0,7) == "decrypt"){
+                // ans = ans.slice(7).split(" ");
+                // let trimedArray = [];
+                // for (let component of ans){
+                //     if (component.trim() != ""){
+                //         trimedArray.push(component);
+                //     }
+                // }
+                // ans = trimedArray;
+
+                // let input = "";
+
+                // let output = "";
+
+                // let password = "";
+
+                // try{
+                //     for (let i = 0; i < ans.length; i+=2){
+                //         if (ans[i] == "--input"){
+                //             input = ans[i+1];
+                //         } else if (ans[i] == "--output"){
+                //             output = ans[i+1];
+                //         } else if (ans[i] == "password"){
+                //             password = ans[i+1];
+                //         }
+                //     }
+
+                //     const algorithm = 'aes-256-gcm';
+                //     const ivLength = 12;
+                //     const saltLength = 64;
+                //     const tagLength = 16;
+                //     const iterations = 10000;
+
+                //     const encryptedFileBuffer = fsf.readFileSync(input);
+
+                //     const salt = encryptedFileBuffer.subarray(0, saltLength - 1);
+                //     const iv = encryptedFileBuffer.subarray(saltLength, saltLength + ivLength - 1);
+                //     // const ciphertext = encryptedFileBuffer.subarray(saltLength + ivLength, encryptedFileBuffer.length - tagLength - 1);
+                //     const authTag = encryptedFileBuffer.subarray(encryptedFileBuffer.length - tagLength);
+                    
+                //     const key = crypto.pbkdf2Sync(password, salt, iterations, 32, "sha512");
+
+                //     const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+                //     decipher.setAuthTag(authTag);
+
+                //     // console.log(salt.toString(), iv.toString(), authTag.toString(), key.toString());
+
+                //     const inputStream = fsf.createReadStream(input, {start: saltLength + ivLength, end: encryptedFileBuffer.length - tagLength - 1});
+                //     const outputStream = fsf.createWriteStream(output);
+
+                //     inputStream.pipe(decipher).pipe(outputStream);
+
+                //     outputStream.on('finish', () => {
+                //         console.log('File decrypted successfully.');
+                //     });
+
+                //     outputStream.on('error', (err) => {
+                //         // This often means an incorrect password/key or a corrupted file (tag mismatch)
+                //         console.error('Decryption failed, likely due to an invalid password or corrupted data:', err.message);
+                //     });
+                // } catch (e) {
+                //     console.log(e, "Operation Failed");
+                // } 
+                console.log("Function not implemented yet");
+                askQuestion(currentDir)
+            } else {
                 console.log("Unknown Command");
 
                 askQuestion(currentDir);
